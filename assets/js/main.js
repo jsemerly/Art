582cbd83f9b1015230067b9e5646d7fcc860025d
jsemerly <jsemerly1@babson.edu>
2026-05-12 13:59:18 -0400

    adding in gallery page functions and basics

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,71 @@ window.addEventListener("scroll", function () {
     }
 
 });
+
+/* =========================
+   GALLERY MODAL
+========================= */
+
+const galleryImages =
+    document.querySelectorAll(".gallery-image");
+
+const modal =
+    document.getElementById("galleryModal");
+
+const modalImage =
+    document.getElementById("modalImage");
+
+const modalTitle =
+    document.getElementById("modalTitle");
+
+const modalCaption =
+    document.getElementById("modalCaption");
+
+const closeButton =
+    document.querySelector(".gallery-close");
+
+/* OPEN MODAL */
+
+galleryImages.forEach(image => {
+
+    image.addEventListener("click", () => {
+
+        modal.classList.add("active");
+
+        modalImage.src = image.src;
+
+        modalTitle.textContent =
+            image.dataset.title;
+
+        modalCaption.textContent =
+            image.dataset.caption;
+
+        document.body.style.overflow = "hidden";
+
+    });
+
+});
+
+/* CLOSE BUTTON */
+
+closeButton.addEventListener("click", () => {
+
+    modal.classList.remove("active");
+
+    document.body.style.overflow = "auto";
+
+});
+
+/* CLICK OUTSIDE */
+
+modal.addEventListener("click", (e) => {
+
+    if (e.target === modal) {
+
+        modal.classList.remove("active");
+
+        document.body.style.overflow = "auto";
+
+    }
+
+});
